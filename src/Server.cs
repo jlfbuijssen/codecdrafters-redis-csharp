de6776b663096d2a42f11c187842514c58b39525
AltCtrler <jlfbuijssen@gmail.com>
2024-11-05 19:45:18 +0100

    Sending PONG

--- src/Server.cs
+++ src/Server.cs
@@ -3,4 +3,14 @@ using System.Net.Sockets;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+Socket socket = server.AcceptSocket(); // wait for client
+
+String msg = "+PONG\r\n";
+char[] chars = msg.ToCharArray();
+byte[] buffer = new byte[chars.Length];
+for (int i = 0; i < chars.Length; i++)
+{
+    buffer[i] = (byte)chars[i];
+}
+socket.Send(buffer);
+socket.Close();
